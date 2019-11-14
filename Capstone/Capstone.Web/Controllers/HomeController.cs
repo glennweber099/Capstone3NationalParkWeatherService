85f10b5a8321715fbf04ec1d132ec238b09a0c03
Katherine Kimes <kbk15@case.edu>
2019-11-14 16:14:52 -0500

    Tried to get session working. Failed.

--- Capstone/Capstone.Web/Controllers/HomeController.cs
+++ Capstone/Capstone.Web/Controllers/HomeController.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Capstone.Web.Models;
 using Capstone.Web.DAO;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
 
 namespace Capstone.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private const string unitSessionKey = "Unit";
+
         private IDetailDAO detailDAO;
         public HomeController(IDetailDAO detailDAO)
         {
@@ -22,18 +26,79 @@ namespace Capstone.Web.Controllers
             return View(parks);
         }
 
+
         public IActionResult Detail(string parkCode)
         {
             ParkModel Park = detailDAO.GetPark(parkCode);
-            //ParkModelVM vm = new ParkModelVM()
-            //{
-            //    park = Park
-            //};
 
+        //    SaveUnitPreference(Park);
             return View(Park);
         }
 
-        public IActionResult GetWeather(string parkCode, string unit)
+        [HttpPost]
+        public IActionResult Detail(ParkModelVM parkModelVM)
+        {
+            HttpContext.Session.SetString("unit", parkModelVM.Unit);
+            SaveUnitPreference(parkModelVM);
+            return RedirectToAction("GetWeather");
+        }
+
+        private void SaveUnitPreference(ParkModelVM parkModelVM)
+        {
+            // Jam that baby back into session
+            string unitJson = JsonConvert.SerializeObject(parkModelVM);
+            HttpContext.Session.SetString(unitSessionKey, unitJson);
+        }
+
+        //private ParkModelVM GetUnitPreference()
+        //{
+        //    // Get the cart from session
+        //    string s = HttpContext.Session.GetString(unitSessionKey);
+        //    ParkModelVM park;
+
+        //    if (s == null || s.Length == 0)
+        //    {
+        //        park = new ParkModelVM();
+        //    }
+        //    else
+        //    {
+        //        // Deserialize to change from a string to an object (DSO)
+        //        park = JsonConvert.DeserializeObject<ParkModelVM>(s);
+        //    }
+        //    return park;
+        //}
+
+        public IActionResult GetWeather(string parkCode)
+        {
+            string s = HttpContext.Session.GetString(unitSessionKey);
+            ParkModelVM park;
+
+            if (s == null || s.Length == 0)
+            {
+                park = new ParkModelVM();
+            }
+            else
+            {
+                // Deserialize to change from a string to an object (DSO)
+                park = JsonConvert.DeserializeObject<ParkModelVM>(s);
+            }
+            if (park.Unit == "Fahrenheit")
+            {
+                return RedirectToAction("GetWeatherFahrenheit");
+            }
+            else
+            {
+                return RedirectToAction("GetWeatherCelsius");
+            }
+        }
+
+        public IActionResult GetWeatherFahrenheit(string parkCode)
+        {
+            IList<Weather> weathers = detailDAO.GetWeather(parkCode);
+            return View(weathers);
+        }
+
+        public IActionResult GetWeatherCelsius(string parkCode)
         {
             IList<Weather> weathers = detailDAO.GetWeather(parkCode);
             return View(weathers);
